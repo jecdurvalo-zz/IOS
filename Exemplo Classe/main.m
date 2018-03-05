08458dcaacc8369b836cec20797d999d5da37200
Usuário Convidado <Guest@l2602micro01.local>
2018-03-05 08:16:10 -0300

    Initial Commit

---
+++ Exemplo Classe/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Exemplo Classe
+//
+//  Created by Usuário Convidado on 05/03/2018.
+//  Copyright © 2018 Joana Durvalo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
