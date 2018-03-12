f93fef8ed010655b2d030d9d1e509614bb080679
Joana Durvalo <j.durvalo@gmail.com>
2018-03-12 08:39:18 -0300

    Initial Commit

---
+++ ExercicioModel/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ExercicioModel
+//
+//  Created by Usuário Convidado on 12/03/2018.
+//  Copyright © 2018 Joana Durvalo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
